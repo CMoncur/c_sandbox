5308161e43c9a59fe8268a6ee0d8692d2a41348a
CMoncur <CMoncur@gmail.com>
2016-12-31 18:59:47 -0800

    babby's first c
    yay

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  printf("The following is a character: %c \n", 'C');
+  printf("The following is an integer: %d \n", 2);
+  printf("The following is a float: %.1f \n", 99.33333);
+  return 0;
+}
